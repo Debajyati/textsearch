f0367b2f0054b291a2ac599a3b8bffcfadcb137d
Debajyati Dey <ddebajyati@gmail.com>
2024-12-09 19:55:44 +0530

    feat: improved search functionality with case sensitivity and order of characters as options

--- bin/main.ml
+++ bin/main.ml
@@ -10,9 +10,17 @@ let file_name =
   let doc = "The file to search in" in
   Arg.(required & pos 1 (some string) None & info [] ~docv:"FILE" ~doc)
 
+let case_sensitive =
+  let doc = "Perform a case-sensitive search" in
+  Arg.(value & flag & info ["c"; "case-sensitive"] ~doc)
+
+let consider_order =
+  let doc = "Consider the order of characters in the search term" in
+  Arg.(value & flag & info ["o"; "consider-order"] ~doc)
+
 (* Core command logic *)
-let run term file =
-  let matches = search_in_file term file in
+let run case_sensitive consider_order term file =
+  let matches = search_in_file ~case_sensitive ~consider_order term file in
   print_matches matches;
   if List.length matches = 0 then
     (Printf.eprintf "No matches found.\n"; exit 1)
@@ -23,11 +31,11 @@ let run term file =
 let cmd =
   let info =
     Cmd.info "textsearch"
-      ~version:"0.1.0"
-      ~doc:"Search for a term in a file"
+      ~version:"0.2.0"
+      ~doc:"Search for a term in a file with options for case sensitivity and character order"
   in
   Cmd.v info
-    Term.(const run $ search_term $ file_name)
+    Term.(const run $ case_sensitive $ consider_order $ search_term $ file_name)
 
 (* Main entry point *)
 let () = Stdlib.exit (Cmd.eval cmd)
--- lib/search.ml
+++ lib/search.ml
@@ -1,23 +1,49 @@
-let search_in_file term file =
+let rec subsequences lst =
+  match lst with
+  | [] -> [[]]
+  | x :: xs ->
+      let subs = subsequences xs in
+      subs @ List.map (fun sub -> x :: sub) subs
+
+let search_in_file ~case_sensitive ~consider_order term file =
+  let normalize s =
+    if case_sensitive then s else String.lowercase_ascii s
+  in
+  let term = normalize term in
+  let term_sorted = List.sort compare (String.to_seq term |> List.of_seq) in
+
+  let contains_term line =
+    let line = normalize line in
+    if consider_order then
+      try
+        let line_len = String.length line in
+        let term_len = String.length term in
+        let rec check_substring i =
+          if i + term_len > line_len then false
+          else if String.sub line i term_len = term then true
+          else check_substring (i + 1)
+        in
+        check_substring 0
+      with _ -> false
+    else
+      let line_sorted = List.sort compare (String.to_seq line |> List.of_seq) in
+      List.exists (fun sublist -> sublist = term_sorted) (subsequences line_sorted)
+  in
+
   let ic = open_in file in
   let rec process_lines line_num acc =
     try
       let line = input_line ic in
-      let new_acc =
-        if String.contains line term.[0] then  (* Use the first character of the term for checking *)
-          (line_num, line) :: acc
-        else
-          acc
-      in
+      let new_acc = if contains_term line then (line_num, line) :: acc else acc in
       process_lines (line_num + 1) new_acc
     with End_of_file ->
       close_in ic;
       List.rev acc
   in
-  process_lines 1 [];;
+  process_lines 1 []
 
 let print_matches matches =
   List.iter
-    (fun (line_num, line) ->
-      Printf.printf "%d: %s\n" line_num line)
+    (fun (line_num, line) -> Printf.printf "%d: %s\n" line_num line)
     matches;;
+
